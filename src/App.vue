ff9a20e7ff2aebae40198e0538c5e1cf8dbfbf25
Caleb Viola <calevime@gmail.com>
2018-06-29 16:03:58 -0400

    Agregado vistas de formulario, nuevo logo, configuración de rúta

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Nav/>
     <router-view/>
   </div>
 </template>
 
+<script>
+import Nav from '@/components/Nav.vue'
+export default {
+  components: {
+    Nav
+  }
+}
+</script>
+
 <style lang="scss">
+* {
+  box-sizing: border-box;
+}
+body{
+  margin: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,57 @@
+<template> 
+  <div class="nav">
+    <img class="logo" src="../assets/forcoli_logo.png">
+    <ul>
+      <li><router-link to="/calendario">Calendario</router-link></li>
+      <li><router-link to="/formularios">Formularios</router-link></li>
+      <li><router-link to="/inicio">Iniciar Sesión</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.nav{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: rgb(66, 66, 66);
+  height: 50px;
+
+  img{
+    height: 40px;
+    width: auto;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    overflow: hidden;
+    height: 100%;
+    li{
+      float: left;
+
+      a{
+        text-decoration: none;
+        color: white;
+        display: block;
+        text-align: center;
+        padding: 16px;  
+
+        &:hover{
+          background-color: #111111;
+        }
+      }
+    }
+  }
+}
+</style>
