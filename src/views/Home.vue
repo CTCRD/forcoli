dd6007e81778228494ece5b5bdf25d0f18a1aa0b
Caleb Viola <calevime@gmail.com>
2018-07-25 14:37:36 -0400

    Normalizar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,10 +7,26 @@ import formulario from '@/components/Formulario.vue'
 
 export default {
   data: () => ({
-    campos: campos
+    campos: normalizar(campos)
   }),
   components: {
     formulario
+  }
+}
+
+function normalizar(campos){
+  let camposCopia = JSON.parse(JSON.stringify(campos))
+  return camposCopia.map(campo => nombreCampo(campo))
+
+  function nombreCampo(campo){
+    let nombre = campo.etiqueta.replace(/[^a-zA-z\s]/g, '').toLowerCase().split(' ').map((el, i) => 
+      i > 0 ? el.charAt(0).toUpperCase() + el.substring(1, el.length) : el
+    ).join('')
+    campo.nombre = nombre
+    if(campo.expandible) nombreCampo(campo.expandible)
+    if(['lista', 'cotejo'].includes(campo.tipo)) 
+      campo.opciones = campo.opciones.map(opcion => nombreCampo(opcion))
+    return campo
   }
 }
 
@@ -18,166 +34,147 @@ export default {
 let campos = [
   {
     etiqueta: "CTC",
-    nombre: "ctc",
     tipo: "lista",
     col: 5,
     opciones: [
-      { etiqueta: "La Barquita", nombre: "barquita" },
-      { etiqueta: "Jimaní", nombre: "jimani" }
+      { etiqueta: "La Barquita" },
+      { etiqueta: "Jimaní" }
     ]
   },
   {
     etiqueta: "Fecha",
-    nombre: "fecha",
     tipo: "fecha",
     col: 3
   },
   {
     etiqueta: "Nombre(s)",
-    nombre: "nombre",
     tipo: "texto",
     col: 2
   },
   {
     etiqueta: "Apellido(s)",
-    nombre: "apellido",
     tipo: "texto",
     col: 2
   },
   {
     etiqueta: "Sexo",
-    nombre: "sexo",
     tipo: "cotejo",
     col: 2,
     opciones: [
-      { etiqueta: "F", nombre: "f" },
-      { etiqueta: "M", nombre: "m" }
+      { etiqueta: "F" },
+      { etiqueta: "M" }
     ]
   },
   {
     etiqueta: "Edad",
-    nombre: "edad",
     tipo: "numero",
     col: 2
   },
   {
     etiqueta: "Nivel de Instrucción",
-    nombre: "nivel",
     tipo: "lista",
     col: 4,
     opciones: [
-      { etiqueta: "Primario", nombre: "primaria" },
-      { etiqueta: "Universitario", nombre: "universitario" },
-      { etiqueta: "Técnico", nombre: "tecnico" },
-      { etiqueta: "Profesional", nombre: "profesional" }
-    ],
-    adicional: true
+      { etiqueta: "Primario" },
+      { etiqueta: "Universitario" },
+      { etiqueta: "Técnico" },
+      { etiqueta: "Profesional" }
+    ]
   },
   {
     etiqueta: "Zona o Comunidad",
-    nombre: "zona",
     tipo: "texto",
     col: 4
   },
   {
     etiqueta: "Identidad Electronica",
-    nombre: "identidad",
     tipo: "texto",
     col: 4
   },
   {
     etiqueta: "Hora de entrada",
-    nombre: "horaEntrada",
     tipo: "hora",
     col: 2
   },
   {
     etiqueta: "Hora de salida",
-    nombre: "horaSalida",
     tipo: "hora",
     col: 2
   },
   {
     etiqueta: "Indique el espacio Maker solicitado",
-    nombre: "espacio",
     tipo: "lista",
     col: 8,
-    expandible: { etiqueta: "Uso", nombre: "uso" },
+    expandible: { etiqueta: "Uso" },
     opciones: [
       { 
         etiqueta: "Producción digital",
-        nombre: "produccion",
         tipo: "lista",
         col: 8,
         multiple: true,
         opciones: [
-          { etiqueta: "Desarrollo de software", nombre: "desarrolloSoftware" },
-          { etiqueta: "Diseño y modelado de producto", nombre: "diseñoModelado" },
-          { etiqueta: "Creación de aplicaciones moviles", nombre: "aplicacionesMoviles" },
-          { etiqueta: "Creación de página Web", nombre: "paginaWeb" },
-          { etiqueta: "Acceso a Información de base tecnológica", nombre: "accesoInfoTec" },
-          { etiqueta: "Creación de diseño grafico", nombre: "diseñoGrafico" },
-          { etiqueta: "Ceación de video juego", nombre: "videoJuego" },
-          { etiqueta: "Edición de video", nombre: "edicionVideo" }
+          { etiqueta: "Desarrollo de software" },
+          { etiqueta: "Diseño y modelado de producto" },
+          { etiqueta: "Creación de aplicaciones moviles" },
+          { etiqueta: "Creación de página Web" },
+          { etiqueta: "Acceso a Información de base tecnológica" },
+          { etiqueta: "Creación de diseño grafico" },
+          { etiqueta: "Ceación de video juego" },
+          { etiqueta: "Edición de video" }
         ]
       },
       {
         etiqueta: "Fabricación digital",
-        nombre: "fabricacion",
         tipo: "lista",
         col: 8,
         multiple: true,
         opciones: [
-          { etiqueta: "Maquinado de piezas", nombre: "maquinadoPiezas" },
-          { etiqueta: "Construcción de prototipos mecánicos inteligentes", nombre: "prototipado" },
-          { etiqueta: "Imprensión 3D", nombre: "impresion3d" },
-          { etiqueta: "Ensamblaje de circuitos", nombre: "circuitos" },
+          { etiqueta: "Maquinado de piezas" },
+          { etiqueta: "Construcción de prototipos mecánicos inteligentes" },
+          { etiqueta: "Imprensión 3D" },
+          { etiqueta: "Ensamblaje de circuitos" },
         ]
       },
       {
         etiqueta: "Workshop",
-        nombre: "workshop",
         tipo: "lista",
         col: 8,
         multiple: true,
         opciones: [
-          { etiqueta: "Construcción de Maqueta", nombre: "maqueta" },
-          { etiqueta: "Trabajos de Carpintería", nombre: "carpinteria" }
+          { etiqueta: "Construcción de Maqueta" },
+          { etiqueta: "Trabajos de Carpintería" }
         ]
       },
       {
         etiqueta: "Coworking",
-        nombre: "coworking",
         tipo: "lista",
         col: 8,
         multiple: true,
         opciones: [
-          { etiqueta: "Formulación de proyectos", nombre: "proyectos" },
-          { etiqueta: "Generación de ideas", nombre: "ideas" },
-          { etiqueta: "Realización de Teletrabjo", nombre: "teletrabajo" }
+          { etiqueta: "Formulación de proyectos" },
+          { etiqueta: "Generación de ideas" },
+          { etiqueta: "Realización de Teletrabjo" }
         ]
       },
       {
         etiqueta: "Estudio de fotografía",
-        nombre: "fotografia",
         tipo: "lista",
         col: 8,
         multiple: true,
         opciones: [
-          { etiqueta: "Sesión fotográfica", nombre: "sesion" },
-          { etiqueta: "Fotografía de productos", nombre: "productos" },
-          { etiqueta: "Retoque Fotográfico", nombre: "retoque" }
+          { etiqueta: "Sesión fotográfica" },
+          { etiqueta: "Fotografía de productos" },
+          { etiqueta: "Retoque Fotográfico" }
         ]
       },
       {
         etiqueta: "Estudio de grabación",
-        nombre: "grabacion",
         tipo: "lista",
         col: 8,
         multiple: true,
         opciones: [
-          { etiqueta: "Grabación de audio", nombre: "grabacion" },
-          { etiqueta: "Producción Músical", nombre: "producción" }
+          { etiqueta: "Grabación de audio" },
+          { etiqueta: "Producción Músical" }
         ]
       }
       
@@ -190,12 +187,11 @@ let campos = [
   },
   {
     etiqueta: "¿Le ha sido util el espacio para lo que necesitaba realizar?",
-    nombre: "evaluacion",
     tipo: "cotejo",
     col: 8,
     opciones: [
-      { etiqueta: "Si", nombre: "si" },
-      { etiqueta: "No", nombre: "no" }
+      { etiqueta: "Si" },
+      { etiqueta: "No" }
     ]
   }
 ]
